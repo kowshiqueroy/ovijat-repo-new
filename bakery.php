237e0f808167e5f1ae9c5e7574339c98c6666265
meherinzaman4444 <135417421+meherinzaman4444@users.noreply.github.com>
2023-09-28 22:53:10 +0600

    tt

--- bakery.php
+++ bakery.php
@@ -15,9 +15,9 @@
                         <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
                             <div class="featured-block d-flex justify-content-center align-items-center">
                                 <a href="#featured-block d-flex justify-content-center align-items-center" class="d-block">
-                                    <img src="images/p/p1.jpg" class="featured-block-image img-fluid" alt="">
+                                    <img src="images/p/to.jpg" class="featured-block-image img-fluid" alt="">
 
-                                  <p class="featured-block-text"><strong>Jhal Muri</strong><br>Details</p>
+                                  <p class="featured-block-text"><strong>Special Toast</strong><br>Details</p>
                                 </a>
                             </div>
                         </div>
@@ -25,8 +25,8 @@
                         <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
                             <div class="featured-block d-flex justify-content-center align-items-center">
                                 <a href="#featured-block d-flex justify-content-center align-items-center" class="d-block">
-                                    <img src="images/p/r.jpg"  class="featured-block-image img-fluid" alt="">
-                                    <p class="featured-block-text"><strong>Aromatic kalijeera Rice</strong><br>Details</p>
+                                    <img src="images/p/ca.jpg"  class="featured-block-image img-fluid" alt="">
+                                    <p class="featured-block-text"><strong>Dry Cake</strong><br>Details</p>
                                 </a>
                             </div>
                         </div>
@@ -34,13 +34,13 @@
                         <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
                             <div class="featured-block d-flex justify-content-center align-items-center">
                                 <a href="#featured-block d-flex justify-content-center align-items-center" class="d-block">
-                                    <img src="images/p/mo.jpg"  class="featured-block-image img-fluid" alt="">
-                                    <p class="featured-block-text"><strong>Oil</strong><br>Details</p>
+                                    <img src="images/p/t.jpg"  class="featured-block-image img-fluid" alt="">
+                                    <p class="featured-block-text"><strong>Sweet Toast</strong><br>Details</p>
                                 </a>
                             </div>
                         </div>
 
-                        <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
+                       <!-- <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
                             <div class="featured-block d-flex justify-content-center align-items-center">
                                 <a href="#featured-block d-flex justify-content-center align-items-center" class="d-block">
                                     <img src="images/p/m.jpg"  class="featured-block-image img-fluid" alt="">
@@ -95,7 +95,7 @@
                                     <p class="featured-block-text"><strong>Sweet Toast</strong><br>Details</p>
                                 </a>
                             </div>
-                        </div>
+                        </div>-->
 
                     </div>
                 </div>
