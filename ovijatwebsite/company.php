926e4ea7c76479c89bf31c44a8238eddf257544b
Kowshique Roy <34647424+kowshiqueroy@users.noreply.github.com>
2023-11-29 21:35:39 +0600

    company img

--- ovijatwebsite/company.php
+++ ovijatwebsite/company.php
@@ -1,29 +1,221 @@
 <?php include 'head.php'; ?>
 
-
-
-
 <section class="cta-section section-padding section-bg">
-                <div class="container">
-                    <div class="row justify-content-center align-items-center">
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
 
-                        <div class="col-lg-12 col-12 ms-auto">
-                            <h2 class="mb-0">
+            <div class="col-lg-12 col-12 ms-auto">
+                <h2 class="mb-0">
 
-                            Ovijat food & beverage industries ltd is an emerging and growing international company in Bangladesh. Ovijat food & beverage industries ltd is a 100% export oriented company starts its journey in 2006. It is very honoring to get the knowledge that the owners of the company was auto rice businessman and it was the vision of Mr. Shamsul Haque to establish food unit that can ensure healthy life of the people around the world. Growth was considered as major Achievement of Mr. Shamsul Haque, Latter his dream was followed by his Elder son Mr. Mostafizur Rahman, Chairman of Ovijat food & beverage industries ltd.
+                    Ovijat food & beverage industries ltd is an emerging and growing international company in
+                    Bangladesh. Ovijat food & beverage industries ltd is a 100% export oriented company starts its
+                    journey in 2006. It is very honoring to get the knowledge that the owners of the company was auto
+                    rice businessman and it was the vision of Mr. Shamsul Haque to establish food unit that can ensure
+                    healthy life of the people around the world. Growth was considered as major Achievement of Mr.
+                    Shamsul Haque, Latter his dream was followed by his Elder son Mr. Mostafizur Rahman, Chairman of
+                    Ovijat food & beverage industries ltd.
 
-Ovijat food believe food & beverage can help to live a healthy & hearty life and we ensure all the ingredients in our products which has a positive effect on the people health. Our Research and development(R & D) department are always working to reduce the portion of the ingredients that can affect your health.
+                    Ovijat food believe food & beverage can help to live a healthy & hearty life and we ensure all the
+                    ingredients in our products which has a positive effect on the people health. Our Research and
+                    development(R & D) department are always working to reduce the portion of the ingredients that can
+                    affect your health.
 
-Our commitment is to meet your daily health nutrition’s with our product and services.
+                    Our commitment is to meet your daily health nutrition’s with our product and services.
 
-Ovijat food & beverage has established many international office around the world to provide the best customer service to the clients and to make position in international market.
+                    Ovijat food & beverage has established many international office around the world to provide the
+                    best customer service to the clients and to make position in international market.
 
-Ovijat has deep-rooted strong commercial alliances with its retail partners in Asia, Middle East, Europe and United States through ensuring quality product and growing reputations. The company has received ISO and HACCAP certification for its management and factories to ensure international standard.
-                            </h2>
-                    
-                </div>
-            </section>
+                    Ovijat has deep-rooted strong commercial alliances with its retail partners in Asia, Middle East,
+                    Europe and United States through ensuring quality product and growing reputations. The company has
+                    received ISO and HACCAP certification for its management and factories to ensure international
+                    standard.
+                </h2>
+
+            </div>
+</section>
+
+<style>
+    div.gallery {
+        border: 1px solid #ccc;
+    }
+
+    div.gallery:hover {
+        border: 1px solid #777;
+    }
+
+    div.gallery img {
+        width: 100%;
+        height: auto;
+    }
+
+    div.desc {
+        padding: 15px;
+        text-align: center;
+    }
+
+    * {
+        box-sizing: border-box;
+    }
+
+    .responsive {
+        padding: 0 6px;
+        float: left;
+        width: 24.99999%;
+    }
+
+    @media only screen and (max-width: 700px) {
+        .responsive {
+            width: 49.99999%;
+            margin: 6px 0;
+        }
+    }
+
+    @media only screen and (max-width: 500px) {
+        .responsive {
+            width: 100%;
+        }
+    }
+
+    .clearfix:after {
+        content: "";
+        display: table;
+        clear: both;
+    }
+</style>
+<section class="cta-section section-padding">
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (1).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
 
 
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (2).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (3).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (4).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+ 
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (5).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (6).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (7).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (8).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (9).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (10).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (11).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (12).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (13).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (14).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (7).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+    <div class="responsive">
+        <div class="gallery">
+            <a target="_blank" href="img_5terre.jpg">
+                <img src="images/c/c (13).jpeg" alt="Cinque Terre" width="600" height="400">
+            </a>
+            <div class="desc"></div>
+        </div>
+    </div>
+   
+
+</section>
 
 <?php include 'foot.php'; ?>
